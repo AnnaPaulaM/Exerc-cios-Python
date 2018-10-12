8a151bbc52c91b54613b392987cf37cff4b8da5d
AnnaPaulaM <44047677+AnnaPaulaM@users.noreply.github.com>
2018-10-12 04:22:41 -0300

    Aula 2

--- Exercício_2.py
+++ Exercício_2.py
@@ -1,1 +1,7 @@
-help('print')
+import math    #importando biblioteca de matemática
+
+r = 5          #raio da esfera
+
+volume = (4/3) * math.pi * (r**3)      #expressão para volume da esfera
+
+print('O volume de uma esfera de raio 5 é:', volume, "u.v.")    #u.v. = unidades de volume
