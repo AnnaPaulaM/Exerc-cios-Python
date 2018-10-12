cae3bcaea5b3b739524421f8f8b2b081e8463cdc
AnnaPaulaM <44047677+AnnaPaulaM@users.noreply.github.com>
2018-10-12 03:51:45 -0300

    Aula 2

--- Exercício_3.py
+++ Exercício_3.py
@@ -1,11 +1,9 @@
-t = 43.5  #tempo dado em minutos
-d = 10    #distância dada em km
+q = 60                          #número de livros
 
-d_mil = d/1.61   #operação para transformar km em milhas
-t_h = t/3600     #operação para transformar tempo de segundos para horas
+preco_desc = 24.95 * 0.6        #preço com desconto em reais
+custo_livros = preco_desc * q  #custo total dos livros em reais
+custo_envio = 3 + (0.75)*(q-1)  #equação para calcular o custo total do envio em reais
 
-v_mil_h = d_mil/t_h    #equação para achar a velocidade em milhas/h
-t_mil_h = 1/v_mil_h    #equação para achar o tempo médio por milha
+custo_total = custo_livros + custo_envio   #custo total em reais
 
-print('A velocidade média em milhas é:', v_mil_h)
-print('O tempo médio por milha é:', t_mil_h)
+print('O custo total para a compra de 60 livros com desconto é de:', custo_total, 'reais.')
---
+++ Exercício_4.py
@@ -0,0 +1,7 @@
+comp_onda = 632.8*(10**-9)    #comprimento de onda em metros
+D = 1.98                      #distância da fenda em metros
+d = 0.250*(10**-3)            #distância entre as fendas em metros
+
+delta_y = (comp_onda * D)/d   #equação para mos consecutivos
+
+print('A distância entre dois máximos consecutivos é:', delta_y, "metros." )
